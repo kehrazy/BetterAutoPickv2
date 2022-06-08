5f75fbb2a9937ed395bd9e54031344b306811c5f
kehrazy <100174039+kehrazy@users.noreply.github.com>
2022-06-08 23:28:43 +0300

    a

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿
+
+using Divine.Service;
+// used: EntityManager
+using Divine.Entity;
+// used: Hero
+using Divine.Entity.Entities.Units.Heroes;
+
+namespace BetterAutoPickv2
+{
+    public sealed class BetterAutoPickv2 : Bootstrapper
+    {
+        public Hero? Me { get; set; }
+
+        protected override void OnActivate()
+        {
+            Me = EntityManager.LocalHero;
+            if (Me == null)
+            {
+                System.Console.WriteLine("hello!");
+            }
+        }
+    }
+
+}
